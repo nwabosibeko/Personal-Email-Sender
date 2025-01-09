9134c7f46fe10d8acc23956ec909a9c5946a020d
nwabosibeko <nwabosibeko17@gmail.com>
2025-01-10 01:48:40 +0200

    Added input to the recepient email, subject, and body.

--- emailer_app/send_email.py
+++ emailer_app/send_email.py
@@ -2,18 +2,22 @@ import os
 from email.message import EmailMessage
 import ssl
 import smtplib
- 
 
-the_pass = os.getenv("python_password")
+
+#Greeting  into the application...
+
+
+
+the_pass = os.getenv("python_password") #the password to my account login (stored in my OS)
 
 
 email_from = "smisosibeko@gmail.com"
-email_to = input("Enter the email of the recepient: ")
 
-email_sub = 'Trying this programm out.'
-email_body = """
-This application is in it's first stage as I will create a GUI going forth
-"""
+
+email_to = input("Enter the email of the recepient: ")
+email_sub = input("Subject: ")
+email_body = input("Body: ")
+
 message = EmailMessage()
 
 message['From'] = email_from
