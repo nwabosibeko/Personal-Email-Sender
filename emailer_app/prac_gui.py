6eaafbef081f70694333190ba68ffa0c37f8b02d
nwabosibeko <nwabosibeko17@gmail.com>
2025-01-10 20:58:29 +0200

    practicing how to add a GUI form using tkinter.

---
+++ emailer_app/prac_gui.py
@@ -0,0 +1,17 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+#system settings
+
+customtkinter.set_appearance_mode("System") #will get the dark or light mode being used.
+customtkinter.set_default_color_theme("blue")
+
+#application frmae
+
+prac_app = customtkinter.CTk()
+prac_app.geometry("720x480")
+prac_app.title("YouTube Downloader")
+
+prac_app.mainloop()
